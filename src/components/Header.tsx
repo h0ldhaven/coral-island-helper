8fc7b39ac1081191dda8f19dea33636b23fe69a2
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-04-04 07:28:51 +0200

    🐛 Fixed: Logo onClick not refreshing the page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        if (location.pathname === '/') {
+            window.location.reload();
+        } else {
+            navigate('/');
+        }
+    };
+
     return(
         <header className='relative overflow-clip'>
             <img
@@ -14,14 +25,13 @@ const Header: React.FC = () => {
             {/* Conteneur Flex pour le logo et le titre */}
             <div className='flex flex-col items-center justify-between w-full h-full px-4 md:flex-row sm:px-8 md:px-16'>
                 {/* Logo à gauche */}
-                <Link to='/' aria-label="Bouton retour a l'acceuil" role='button'>
-                    <img
-                        className='md:left-[2.5vw] md:absolute md:top-1/2 md:-translate-y-1/2 w-auto max-w-36 sm:max-w-44 lg:max-w-48 h-auto z-50'
-                        src={`${import.meta.env.BASE_URL}images/icon.webp`}
-                        alt='Logo de coral island'
-                        aria-hidden='true'
-                    />
-                </Link>
+                <img
+                    onClick={handleLogoClick}
+                    className='md:left-[2.5vw] md:absolute md:top-1/2 md:-translate-y-1/2 w-auto max-w-36 sm:max-w-44 lg:max-w-48 h-auto z-50'
+                    src={`${import.meta.env.BASE_URL}images/icon.webp`}
+                    alt='Logo de coral island'
+                    aria-hidden='true'
+                />
 
                 {/* Titre centré sous le logo */}
                 <div className='text-shadow-custom flex flex-col items-center justify-center w-full md:pt-24 pb-10 md:pb-16 leading-none text-white font-comfortaa text-[3em] z-10'>
