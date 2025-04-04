4146e875839af2401f3b4992b5bf65bad3954931
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-04-04 06:37:46 +0200

    🚨 Fixed lint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return(
-        <header className="relative overflow-clip">
+        <header className='relative overflow-clip'>
             <img
-                className="absolute object-cover object-center w-full h-full -z-10"
+                className='absolute object-cover object-center w-full h-full -z-10'
                 src={`${import.meta.env.BASE_URL}images/background_1.webp`}
-                alt="Fond de coral island"
-                aria-hidden="true"
+                alt='Fond de coral island'
+                aria-hidden='true'
             />
 
             {/* Conteneur Flex pour le logo et le titre */}
-            <div className="flex flex-col items-center justify-between w-full h-full px-4 md:flex-row sm:px-8 md:px-16">
+            <div className='flex flex-col items-center justify-between w-full h-full px-4 md:flex-row sm:px-8 md:px-16'>
                 {/* Logo à gauche */}
-                <Link to="/" aria-label="Bouton retour a l'acceuil" role="button">
+                <Link to='/' aria-label="Bouton retour a l'acceuil" role='button'>
                     <img
-                        className="md:left-[2.5vw] md:absolute md:top-1/2 md:-translate-y-1/2 w-auto max-w-36 sm:max-w-44 lg:max-w-48 h-auto z-50"
+                        className='md:left-[2.5vw] md:absolute md:top-1/2 md:-translate-y-1/2 w-auto max-w-36 sm:max-w-44 lg:max-w-48 h-auto z-50'
                         src={`${import.meta.env.BASE_URL}images/icon.webp`}
-                        alt="Logo de coral island"
-                        aria-hidden="true"
+                        alt='Logo de coral island'
+                        aria-hidden='true'
                     />
                 </Link>
 
--- src/components/gameplay/Enchanting.tsx
+++ src/components/gameplay/Enchanting.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Enchanting: React.FC = () => {
     return (
-        <section className="flex flex-col items-center justify-baseline min-h-screen relative p-4">
+        <section className='flex flex-col items-center justify-baseline min-h-screen relative p-4'>
 
             Enchanting
         </section>
