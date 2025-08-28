177178e245189ee871fe391f18a2b7ef0d20d304
h0ldhaven <30150325+h0ldhaven@users.noreply.github.com>
2025-08-28 18:24:05 +0200

    Enhance commit ci (#19)
    * 📦 Update package
    
    * 👷 Update Commit CI
    
    * 👷 update commit ci install
    
    * 👷 Add security step
    
    * 👷 Update build step o nsecurity scan
    
    * 👷 Update commit-ci
    
    * ✨ Unit test in code quality check
    
    * 👷 Update check types fore CodeQL
    
    * 👷 Add main branch to pull request
    
    ---------
    
    Co-authored-by: h0ldhaven <h0ldhaven@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
     base: '/coral-island-helper/',
     plugins: [
@@ -12,6 +11,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './vitest.setup.ts',
+        setupFiles: './tests/setup/vitest.setup.ts',
+        include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'],
     },
 });
