7cde7a63abffae1588efc6b335ee7078c19fc2dc
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-08-29 11:37:38 +0200

    🚧 update Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
